8160f1ad6d9ac422cc682229bd847908be591c54
Hironobu Kinjo <hironobukinjo@gmail.com>
2024-02-19 13:58:58 +0900

    ユーザースニペットの追加

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField("名前", max_length=50)
+
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Employee_detail", kwargs={"pk": self.pk})
