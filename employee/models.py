62e4057ffab1efd8590c8ca301f6d9ac19cbc059
Hironobu Kinjo <hironobukinjo@gmail.com>
2024-02-19 12:15:17 +0000

    modelの修正

--- employee/models.py
+++ employee/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-from django.urls import reverse
 
-class Employee(models.Model):
-    name = models.CharField("名前", max_length=50)
-
-    class Meta:
-        verbose_name = "Employee"
-        verbose_name_plural = "Employees"
+class Department(models.Model):
+    """ 部署 """
+    name = models.CharField("部署名", max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse("Employee_detail", kwargs={"pk": self.pk})
+class Employee(models.Model):
+    """ 従業者 """
+    name = models.CharField("従業者名", max_length=50)
+    department = models.ForeignKey(Department, verbose_name="所属部署", on_delete=models.SET_NULL, blank=True, null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
