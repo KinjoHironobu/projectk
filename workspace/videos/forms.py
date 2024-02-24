9fcbeff2e97f03b9a6b70c4fc91d6ae611946f73
Hironobu Kinjo <hironobukinjo@gmail.com>
2024-02-24 18:02:05 +0900

    HLS対応

--- workspace/videos/forms.py
+++ workspace/videos/forms.py
@@ -10,15 +10,17 @@ class VideoCreateForm(forms.ModelForm):
         # 少しツウな書き方
         widgets = {
             'title': forms.TextInput(attrs={  # <input type="text" class="form-control"
-                'class': 'form-control',
+                'class': 'form-control mb-3',
             }),
             'description': forms.Textarea(attrs={  # <textarea class="form-cotrol"
-                'class': 'form-control',
+                'class': 'form-control mb-3',
             }),
             'thumbnail': forms.ClearableFileInput(attrs={  # <input type="file" class="form-control-file"
-                'class': "form-control-file",
+                'class': "form-control mb-3",
+                'type': "file"
             }),
             'upload': forms.ClearableFileInput(attrs={
-                'class': "form-control-file",
+                'class': "form-control mb-3",
+                'type': "file"
             }),
         }
--- workspace/videos/models.py
+++ workspace/videos/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+import subprocess
+import os
 
 class Video(models.Model):
     """動画"""
@@ -7,8 +10,40 @@ class Video(models.Model):
     description = models.TextField('説明(空欄可)', blank=True)
     thumbnail = models.ImageField('サムネイル(空欄可)', upload_to='thumbnails/', null=True, blank=True)  # /media/thumbnails/ファイル名
     upload = models.FileField('ファイル', upload_to='uploads/%Y/%m/%d/')  # /media/uploads/2018/3/20/ファイル名
+    hls_path = models.FileField('HLSファイル', blank=True, null=True)
     created_at = models.DateTimeField('作成日', auto_now_add=True)  # default=timezone.nowと違い、入力欄は表示されない
     updated_at = models.DateTimeField('更新日', auto_now=True)  # 更新するたびにその日時が格納される
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.hls_path:
+            self.convert_to_hls()
+
+    def convert_to_hls(self):
+        # アップロードされたファイルのパスを取得
+        input_file = self.upload.path
+        
+        # 出力先ディレクトリをアップロードされたファイルと同じにする
+        output_dir = os.path.dirname(input_file)
+        output_file_base = os.path.splitext(os.path.basename(input_file))[0]
+        hls_output_path = os.path.join(output_dir, f"{output_file_base}_hls")
+        
+        # 出力ディレクトリが存在しない場合は作成
+        os.makedirs(hls_output_path, exist_ok=True)
+
+        # ffmpegを使用して動画をHLS形式に変換
+        command = [
+            'ffmpeg', '-i', input_file,
+            '-profile:v', 'baseline', '-level', '3.0',
+            '-s', '640x360', '-start_number', '0',
+            '-hls_time', '5', '-hls_list_size', '0',
+            '-f', 'hls', os.path.join(hls_output_path, 'index.m3u8')
+        ]
+        subprocess.run(command, check=True)
+        
+        # HLSパスを更新
+        self.hls_path = os.path.join(hls_output_path, 'index.m3u8').lstrip(settings.MEDIA_ROOT)
+        super().save(update_fields=['hls_path'])
